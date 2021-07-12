1416fd7089533e384c79965e64236296fd787519
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-07-11 22:13:07 -0400

    Code added.

--- Operator_Overloading.cpp
+++ Operator_Overloading.cpp
@@ -1,5 +1,3 @@
-#include<iostream>
-using namespace std;
 
 // ambiguous - unclear, when multiple functions with same name are
 // specified and compiler don't know what function to call.
@@ -8,12 +6,28 @@ using namespace std;
 // Operand: an input for an operation such as 5, "hi", 'a'
 // Rules: Operators act upon operands. Operation type is determined from the operator and operands.
 
+// --------------------------------------- Operator Overloading
+// "5" + "7" == "57"
+// the + operator is overloaded for the string class
+// IT IS THE OPERANDS THAT DETERMINES WHICH VERSION OF + IS CALLED.
+// If it's 5 + 7 == 12, it's just a sum of two integers.
 
-int main() {
+// Three ways to overload operators:
+// Member, non-member, friend
+
+// Member functions: are binary, they have two operands. First operand is the calling object.
+// second one will be a parameter for the function.
+
+// Non-Member functions: Both operands will be parameters. Parameters means objects
+// that are of the type of class which we are overloading these operators for.
+
+// Friend Functions
+// Friend Functions are non-member functions that are labeled as friend functions.
+// As a result, they are given member access to the class.
+// Unlike a non-member function, you'll have direct access to class members.
 
 
 
 
-    cout << endl;
-    return 0;
-}
+
+
---
+++ Unary_Member_Overload.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+// --------------------------------------- Unary Member Overload
+// Operator with one operand.
+
+// -(minus) unary operator negates a single operand
+// Number a{3.5};
+// cout << -a; prints -3.5
+// cout << a;  prints 3.5
+
+// In Unary Overload, you don't modify the calling object, instead you return a negated equivalent.
+
+class Number {
+public:
+    Number(): Number(0) {}          // 1st constructor
+    Number(double n): num(n) {}        // 2nd Constructor
+    void output() const {cout << num << "\n";}
+
+    // unary - member operator overload
+    // return a temporary object which is a negated copy of the calling object
+    Number operator-() const { // This is the overloaded negation operator. Operator followed by the symbol.
+        return Number(-num);   // Temporary object - by end of this line, it goes out of scope.
+            // Calling 2nd constructor Number()
+    }
+private:
+    double num;
+};
+
+int main() {
+    // negation upon integer, note that the original integer is not modified.
+    int x = 5;
+    cout << -x << "\n";
+    cout << x << "\n\n";
+
+    // a is the calling object, note that a is not modified.
+    Number a{3.25};
+    (-a).output();  // negating because we have one operand and a negation symbol.
+    a.output();
+}
