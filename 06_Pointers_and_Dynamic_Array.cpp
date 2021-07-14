8a0544df31427c7f2328e2be139d748200a856b4
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-07-13 23:09:14 -0400

    Code added.

--- 06_Pointers_and_Dynamic_Array.cpp
+++ 06_Pointers_and_Dynamic_Array.cpp
@@ -18,9 +18,78 @@ int main() {
     // *p can modify the value of d
     *p = 10;
     cout << d << "\n";      // Prints 10
-    cout << *p << "\n";     // Prints 10
+    cout << *p << "\n\n";   // Prints 10
 
     // -------------------------------------------------------- NULLPTR
+    // Pointer currently points to nothing.
+
+    cout << "---------------------- nullptr:\n";
+    // int *b;                              // p currently stores garbage
+
+    int *b = nullptr;                       // initialize int pointer b with the value of nullptr
+    cout << "b: " << b << "\n";             // prints b = 0
+                                            //
+                                            // if (b == nullptr) {      // can test if p points to nothing
+                                            //      run some code
+                                            // }
+    if (b == nullptr) {
+        cout << "b currently points to nothing.\n\n";
+    }
+
+    // -------------------------------------------------------- Pointer Syntax
+    cout << "---------------------- Pointer Syntax:\n";
+
+    int x = 5;
+    int z = 20;
+    int *y = nullptr;       // initialize the integer pointer y
+    y = &x;                 // assign y to the memory address of x, will return the value of x
+    *y = 10;                // dereferences y to access and modify the value of x
+
+    cout << *y << "\n";     // Prints 10
+    cout << x << "\n";      // Prints 10
+    cout << y << "\n";      // Prints memory address of x
+    cout << &x << "\n\n";   // Prints memory address of x
+
+    // just like we can change the value of strings or ints, we can change the value of pointers.
+    y = &z;                 // y no longer points to x, it returns z
+    cout << z << "\n";      // Prints 20
+    cout << *y << "\n\n";   // Prints 20
+
+    double *m, n;           // declare a double pointer m and a double n
+    double h, *i;           // declare a double h and a double pointer i
+    double *j, *k;          // declare two double pointer j and k
+
+
+    // -------------------------------------------------------- Pointer Equivalence
+    cout << "---------------------- Pointer Equivalence:\n";
+
+    double q = 3.14;
+    double *r = &q;         // initialize the double pointer r
+    double *s = &q;         // initialize the double pointer s
+
+    //if (&r == &s) {cout << "r & s are NOT the same variables, condition is always false";}
+    if (r == s) {
+        cout << "r & s point to the same variable.\n";
+    }
+    if (*r == *s) {
+        cout << "r & s point to variables with the same value.\n\n";
+    }
+
+    cout << q << "\n";      // Prints 3.14
+    cout << *r << "\n";     // Prints 3.14
+    cout << *s << "\n\n";   // Prints 3.14
+
+    cout << &q << "\n";     // Prints a different memory address
+    cout << &r << "\n";     // Prints a different memory address
+    cout << &s << "\n\n";   // Prints a different memory address
+
+    cout << r << "\n";      // Prints the memory address of q
+    cout << s << "\n";      // Prints the memory address of q
+
+
+
+
+
 
 
 
