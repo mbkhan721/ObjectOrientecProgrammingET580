b373db32c2efc0a7604cee6564159f86065827dd
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-07-21 11:35:28 -0400

    Code added.

--- Exam2_Question3.cpp
+++ Exam2_Question3.cpp
@@ -1,4 +1,4 @@
-
+/*
 // Muhammad B. Khan
 // Exam 2 - Question 3
 // ET580 - Summer 2
@@ -8,10 +8,19 @@ using namespace std;
 
 // 3) --------------------------------------------------------------------------
 class Team {
+private:
+    string name;
+    string *members;
+    int size;
 public:
+    Team() {name = "TBD", members = nullptr, size = 0;}
+    Team(string Name) {name = Name; members = nullptr; size = 0;}
+
+    void setName(string Name) {name = Name;}
+    string getName() {return name;}
+    int getSize() {return size;}
     
 };
-
 
 // *********************************************************************************
 // **************************      MAIN FUNCTION    ********************************
@@ -21,9 +30,14 @@ int main() {
     // 3) --------------------------------------------------------------------------
     cout << "\nMuhammad B. Khan\nExam 2\nET580 - Summer 2\n\n------------ Question 3: \n";
 
-
-
+    Team a,b("Mets");
+    cout<<a.getName()<<endl; // print TBD
+    a.setName("Yankee");
+    cout<<a.getName()<<endl; // print Yankee
+    cout<<b.getName()<<endl; // print Mets
+    cout<<a.getSize()<<endl; // print 0
 
     cout << endl;
     return 0;
 }
+*/
