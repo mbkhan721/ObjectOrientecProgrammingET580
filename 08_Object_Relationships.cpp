81452b45befa7d9d5d54d5faa747e2aa7c5a94c8
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-07-26 20:20:59 -0400

    Code added.

--- 08_Object_Relationships.cpp
+++ 08_Object_Relationships.cpp
@@ -2,6 +2,32 @@
 #include <iostream>
 using namespace std;
 
+// Composition is a structure with two or more classes. Ex: Class A had a member of Class B.
+// OR Class B is part of class A.
+// OR we can say that we have a human class and a heart class.
+// Human has-a heart or heart is part of human.
+// This relationship is unique, if the heart dies, the human dies or vice versa.
+// The scope and lifetime of both are linked.
+
+class Arm {
+private:
+    string position;
+public:
+    Arm(): Arm(""){}
+    Arm(string p): position(p) {}
+    string getPosition() {return position;}
+};
+class Person {
+private:
+    string name;
+    Arm left;
+    Arm right;
+public:
+    Person(): Person("Anonymous") {}
+    Person(string n): name(n), left("left arm"), right("right arm") {}
+
+};
+
 
 
 // *********************************************************************************
