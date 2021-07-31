f4d2ae5cf1b1d51b48843b4e5b6f62b3e069730e
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-07-31 15:59:14 -0400

    Code added.

---
+++ Homework4_Q1.cpp
@@ -0,0 +1,88 @@
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+
+//
+// Created by Muhammad B. Khan
+// ET580 Summer2
+// Homework 4 - Question 1
+//
+// 1) --------------------------------------------------------------------------
+class Pet {
+public:
+    string *name;
+    int age{};
+    double weight{};
+
+    Pet(): Pet("", 0, 0.0){}
+    Pet(string n, int a, double w): name(new string(n)), age(a), weight(w) {}
+
+    string getName() const {return *name;}
+    int getAge() const {return age;}
+    double getWeight() const {return weight;}
+
+    string& setName(string n) const {*name = n;}
+    static string getLifespan() {return "unknown lifespan";}
+
+    // The big three
+    Pet(const Pet &p);
+    Pet& operator = (const Pet &p);
+    ~Pet();
+
+};
+Pet::Pet(const Pet &p): name(new string(*(p.name))) {
+    name = p.name;
+    age = p.age;
+    weight = p.weight;
+    cout << "==> (Pet) copy constructor was called\n";
+}
+
+Pet& Pet::operator = (const Pet &p) {
+    if (this != &p) {
+        *name = *(p.name);
+        age = p.age;
+        weight = p.weight;
+    }
+    cout << "==> (Pet) Assignment operator was called\n";
+    return *this;
+}
+
+Pet::~Pet() {
+    delete name;
+    cout << "==> (Pet) Destructor was called\n";
+}
+
+
+// *********************************************************************************
+// **************************      MAIN FUNCTION    ********************************
+// *********************************************************************************
+
+int main() {
+
+    // 1) --------------------------------------------------------------------------
+    cout << "\nMuhammad B. Khan - Homework 4\nET580 - Summer\n\n------------ Problem 1: \n";
+
+    Pet p("Kitty",2,10);
+    cout<<p.getName()<<endl;
+    cout<<p.getAge()<<endl;
+    cout<<p.getWeight()<<endl;
+    cout<<p.getLifespan()<<endl;
+    Pet p1 = p, p2;
+    p1.setName("Doggy");
+    cout<<p1.getName()<<endl;
+    cout<<p1.getAge()<<endl;
+    cout<<p1.getWeight()<<endl;
+    cout<<p1.getLifespan()<<endl;
+    p2 = p1;
+    p2.setName("Teddy");
+    cout<<p2.getName()<<endl;
+    cout<<p2.getAge()<<endl;
+    cout<<p2.getWeight()<<endl;
+    cout<<p2.getLifespan()<<endl;
+
+
+    cout << endl;
+    return 0;
+}
+*/
---
+++ Homework4_Q2.cpp
@@ -0,0 +1,120 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+//
+// Created by Muhammad B. Khan
+// ET580 Summer2
+// Homework 4 - Question 2
+//
+// 2) --------------------------------------------------------------------------
+class Pet {
+public:
+    string *name;
+    int age{};
+    double weight{};
+
+    Pet(): Pet("", 0, 0.0){}
+    Pet(string n, int a, double w): name(new string(n)), age(a), weight(w) {}
+
+    string getName() const {return *name;}
+    int getAge() const {return age;}
+    double getWeight() const {return weight;}
+
+    string& setName(string n) const {*name = n;}
+    static string getLifespan() {return "unknown lifespan";}
+
+    // The big three
+    Pet(const Pet &p);
+    Pet& operator = (const Pet &p);
+    ~Pet();
+
+};
+Pet::Pet(const Pet &p): name(new string(*(p.name))) {
+    name = p.name;
+    age = p.age;
+    weight = p.weight;
+    cout << "==> (Pet) copy constructor was called\n";
+}
+
+Pet& Pet::operator = (const Pet &p) {
+    if (this != &p) {
+        *name = *(p.name);
+        age = p.age;
+        weight = p.weight;
+    }
+    cout << "==> (Pet) Assignment operator was called\n";
+    return *this;
+}
+
+Pet::~Pet() {
+    delete name;
+    cout << "==> (Pet) Destructor was called\n";
+}
+
+class Dog : public Pet {
+private:
+    string *breed;
+public:
+    Dog() {}
+    Dog(string n, int a, double w, string b): breed (new string(b)), Pet(n, a, w){}
+
+    string getBreed() {return *breed;}
+    string& setBreed(string b) {*breed = b;}
+
+    // Copy constructor
+    Dog(const Dog &c): Pet(c), breed (new string(*(c.breed))) {
+        cout << "==> (Dog) copy constructor was called\n";
+    }
+
+    // assignment operator overload
+    Dog& operator = (const Dog &c) {
+        cout << "==> (Dog) Assignment operator was called\n";
+        if (this != &c) {
+            Pet::operator=(c);
+            *breed = *(c.breed);
+        }
+        return *this;
+    }
+
+    ~Dog() {
+        delete breed;
+    }
+};
+
+
+// *********************************************************************************
+// **************************      MAIN FUNCTION    ********************************
+// *********************************************************************************
+
+int main() {
+
+    // 2) --------------------------------------------------------------------------
+    cout << "\nMuhammad B. Khan - Homework 4\nET580 - Summer\n\n------------ Problem 2: \n";
+
+    Dog d("Doggy",2,10,"Boxer");
+    cout<<d.getName()<<endl;
+    cout<<d.getAge()<<endl;
+    cout<<d.getWeight()<<endl;
+    cout<<d.getBreed()<<endl;
+    cout<<d.getLifespan()<<endl;
+    Dog d1 = d,d2;
+    d1.setBreed("Akita");
+    cout<<d1.getName()<<endl;
+    cout<<d1.getAge()<<endl;
+    cout<<d1.getWeight()<<endl;
+    cout<<d1.getBreed()<<endl;
+    cout<<d1.getLifespan()<<endl;
+    d2 = d1;
+    d2.setName("King");
+    cout<<d2.getName()<<endl;
+    cout<<d2.getAge()<<endl;
+    cout<<d2.getWeight()<<endl;
+    cout<<d2.getBreed()<<endl;
+    cout<<d2.getLifespan()<<endl;
+
+
+    cout << endl;
+    return 0;
+}
